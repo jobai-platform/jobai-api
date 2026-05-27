f829c142b8e7839f0123195dfa8d50496d0653e1
Richardson <rpsantosvix@gmail.com>
2026-05-27 20:57:03 +0200

    feat(database): add vector extension and create candidate_embeddings table

--- migrations/versions/2026_05_07_1643-aac3238ce036_create_vector_embeddings_tables_with_.py
+++ migrations/versions/2026_05_07_1643-aac3238ce036_create_vector_embeddings_tables_with_.py
@@ -19,6 +19,8 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
+    op.execute("CREATE EXTENSION IF NOT EXISTS vector;")
+
     # --- candidate_embeddings ---
     op.create_table(
         'candidate_embeddings',
